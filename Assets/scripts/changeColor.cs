ee08dcb86b56e9adc1c0f5c5f24e127d0f7fa7ad
marticoma <46682113+marticoma@users.noreply.github.com>
2020-05-04 12:13:55 +0200

    Initial commit

---
+++ Assets/scripts/changeColor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+
+public class changeColor : MonoBehaviour
+{
+
+    public Material sphereMaterial;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        if(pos.magnitude > 100 && pos.magnitude <150) sphereMaterial.SetColor("_Color", Color.white);
+        else if(pos.magnitude > 150 && pos.magnitude < 200) sphereMaterial.SetColor("_Color", Color.red);
+        else if (pos.magnitude > 200 && pos.magnitude < 250) sphereMaterial.SetColor("_Color", Color.blue);
+        else if (pos.magnitude > 250 && pos.magnitude < 300) sphereMaterial.SetColor("_Color", Color.green);
+        else if (pos.magnitude > 300) sphereMaterial.SetColor("_Color", Color.yellow);
+    }
+}
---
+++ Assets/scripts/sphereControll.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class sphereControll : MonoBehaviour
+{
+    public GameObject sphere;
+    public int initX;
+    public int initY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        sphere.transform.position = transform.position + new Vector3(initX, initY, 0);
+    }
+}
